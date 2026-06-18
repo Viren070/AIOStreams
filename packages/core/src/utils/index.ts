66453b5b2e14b7507640089fb00861701cac5ff0
Viren070 <viren070@protonmail.com>
2026-06-18 14:19:25 +0100

    feat(anime-database): refactor, add new source, fix fribbs parsing (#1026)

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -22,7 +22,7 @@ export * from './extras.js';
 export * from '../poster/index.js';
 export * from './distributed-lock.js';
 export * from './id-parser.js';
-export * from './anime-database.js';
+export * from '../anime-database/index.js';
 export * from './regex.js';
 export * from './general.js';
 export * from './seadex.js';
